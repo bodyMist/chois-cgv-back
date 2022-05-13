84bc5b25551552d46a991a2492cc075eb0883d45
bodymist <sky834459@gmail.com>
2022-05-13 11:59:32 +0900

    alter Seat attributes name due to db error

--- src/main/java/choiscgvback/cgv/domain/Seat.java
+++ src/main/java/choiscgvback/cgv/domain/Seat.java
@@ -7,9 +7,9 @@ public class Seat {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "SEAT_ID")
     private Long id;
-    @Column(name = "ROW_NUMBER")
+    @Column(name = "ROW_NO")
     private Integer row;
-    @Column(name = "COLUMN_NUMBER")
+    @Column(name = "COLUMN_NO")
     private Integer column;
     @Column(name = "POSSIBILITY")
     private Boolean possibility;    // 0: 사용가능 1: 사용불가능
