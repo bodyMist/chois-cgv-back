effb563d4da7c2691d79a8d71c34513be12bfcbb
bodyMist <sky834459@gmail.com>
2022-05-16 00:56:19 +0900

    add MainPage request

--- src/main/java/choiscgvback/cgv/repository/MovieRepository.java
+++ src/main/java/choiscgvback/cgv/repository/MovieRepository.java
@@ -1,13 +1,24 @@
 package choiscgvback.cgv.repository;
 
 import choiscgvback.cgv.domain.Movie;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
     List<Movie> findAll();
-    // Create&Update
-    Movie saveAndFlush(Movie newMovie);
+    Optional<Movie> findById(Long id);
+
+    // 메인페이지, 현재 상영중인 영화를 평점 내림차순으로 조회, page_size=5
+    @Query("SELECT i.movie.id,SUM(i.score) FROM REVIEWS i " +
+            "WHERE i.movie.id IN " +
+            "(SELECT DISTINCT i.id FROM MOVIES i " +
+            "LEFT OUTER JOIN Running r ON i.id = r.movie.id AND r.startTime > :now) " +
+            "GROUP BY i.movie.id ORDER BY SUM(i.score) DESC")
+    Slice<Movie> findByRunningAndReleaseDate(LocalDateTime now, Pageable pageable);
 }
---
+++ src/main/java/choiscgvback/cgv/service/MovieService.java
@@ -0,0 +1,28 @@
+package choiscgvback.cgv.service;
+
+import choiscgvback.cgv.domain.Movie;
+import choiscgvback.cgv.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MovieService {
+    private final MovieRepository movieRepository;
+    public Movie saveOne(Movie newMovie) {return movieRepository.save(newMovie);}
+    public Movie findOne(Long movieId){return movieRepository.findById(movieId).get();}
+
+    // 메인페이지, 현재 상영작 예메순|평점순 조회(page_size=5)
+    public Slice<Movie> findRunningMovie(int page){
+        Pageable pageRequest = PageRequest.of(page,5, Sort.by(Sort.Direction.ASC, ""));
+        return movieRepository.findByRunningAndReleaseDate(LocalDateTime.now(), pageRequest);
+    }
+}
