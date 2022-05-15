610a1391e5cdc94187049a5819c377177b3594aa
bodyMist <sky834459@gmail.com>
2022-05-16 00:50:14 +0900

    update a query method on MemberRepository

--- src/main/java/choiscgvback/cgv/repository/MemberRepository.java
+++ src/main/java/choiscgvback/cgv/repository/MemberRepository.java
@@ -3,10 +3,10 @@ package choiscgvback.cgv.repository;
 import choiscgvback.cgv.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     // 로그인
-    List<Member> findMemberByAccountEqualsAndPasswordEquals(String account, String password);
-
+    Member findMemberByAccountEqualsAndPasswordEquals(@NotNull String account, @NotNull String password);
 }
