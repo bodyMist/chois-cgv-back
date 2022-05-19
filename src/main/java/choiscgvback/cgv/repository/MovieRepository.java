6ee889224995e09d8ac251db956d0ee0f487ed4c
bodyMist <sky834459@gmail.com>
2022-05-19 19:57:32 +0900

    add a findRunningMovie function not including pagable

--- src/main/java/choiscgvback/cgv/repository/MovieRepository.java
+++ src/main/java/choiscgvback/cgv/repository/MovieRepository.java
@@ -15,10 +15,17 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     Optional<Movie> findById(Long id);
 
     // 메인페이지, 현재 상영중인 영화를 평점 내림차순으로 조회, page_size=5
-    @Query("SELECT i.movie.id,SUM(i.score) FROM REVIEWS i " +
+    @Query("SELECT i.movie.id, AVG(i.score) FROM REVIEWS i " +
             "WHERE i.movie.id IN " +
             "(SELECT DISTINCT i.id FROM MOVIES i " +
             "LEFT OUTER JOIN Running r ON i.id = r.movie.id AND r.startTime > :now) " +
             "GROUP BY i.movie.id ORDER BY SUM(i.score) DESC")
-    Slice<Movie> findByRunningAndReleaseDate(LocalDateTime now, Pageable pageable);
+    Slice<Movie> findByRunningAndReleaseDateWithPageable(LocalDateTime now, Pageable pageable);
+    // 메인페이지, 현재 상영중인 영화를 평점 내림차순으로 조회, pagination X
+    @Query("SELECT i.movie.id, AVG(i.score) FROM REVIEWS i " +
+            "WHERE i.movie.id IN " +
+            "(SELECT DISTINCT i.id FROM MOVIES i " +
+            "LEFT OUTER JOIN Running r ON i.id = r.movie.id AND r.startTime > :now) " +
+            "GROUP BY i.movie.id ORDER BY SUM(i.score) DESC")
+    List<Movie> findByRunningAndReleaseDate(LocalDateTime now);
 }
--- src/main/java/choiscgvback/cgv/service/MovieService.java
+++ src/main/java/choiscgvback/cgv/service/MovieService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional
@@ -23,6 +24,11 @@ public class MovieService {
     // 메인페이지, 현재 상영작 예메순|평점순 조회(page_size=5)
     public Slice<Movie> findRunningMovie(int page){
         Pageable pageRequest = PageRequest.of(page,5, Sort.by(Sort.Direction.ASC, ""));
-        return movieRepository.findByRunningAndReleaseDate(LocalDateTime.now(), pageRequest);
+        return movieRepository.findByRunningAndReleaseDateWithPageable(LocalDateTime.now(), pageRequest);
     }
+    // 메인페이지, 현재 상영작 예메순|평점순 조회
+    public List<Movie> findRunningMovie(){
+        return movieRepository.findByRunningAndReleaseDate(LocalDateTime.now());
+    }
+
 }
