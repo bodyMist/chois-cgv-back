eb38dc20e08c967890e500904e0896240c108a22
bodymist <sky834459@gmail.com>
2022-05-20 15:22:37 +0900

    modify select movies on main page and add select movies on movie page

--- src/main/java/choiscgvback/cgv/repository/MovieRepository.java
+++ src/main/java/choiscgvback/cgv/repository/MovieRepository.java
@@ -15,17 +15,27 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     Optional<Movie> findById(Long id);
 
     // 메인페이지, 현재 상영중인 영화를 평점 내림차순으로 조회, page_size=5
-    @Query("SELECT i.movie.id, AVG(i.score) FROM REVIEWS i " +
-            "WHERE i.movie.id IN " +
+    @Query("SELECT avg(r.score), m.id, m.name, m.posterURL from MOVIES m " +
+            "LEFT OUTER JOIN REVIEWS r on m.id = r.movie.id " +
+            "WHERE m.id IN " +
             "(SELECT DISTINCT i.id FROM MOVIES i " +
             "LEFT OUTER JOIN Running r ON i.id = r.movie.id AND r.startTime > :now) " +
-            "GROUP BY i.movie.id ORDER BY SUM(i.score) DESC")
+            "GROUP BY m.id ORDER BY AVG(r.score) DESC")
     Slice<Movie> findByRunningAndReleaseDateWithPageable(LocalDateTime now, Pageable pageable);
     // 메인페이지, 현재 상영중인 영화를 평점 내림차순으로 조회, pagination X
-    @Query("SELECT i.movie.id, AVG(i.score) FROM REVIEWS i " +
-            "WHERE i.movie.id IN " +
+    @Query("SELECT avg(r.score), m.id, m.name, m.posterURL from MOVIES m " +
+            "LEFT OUTER JOIN REVIEWS r on m.id = r.movie.id " +
+            "WHERE m.id IN " +
             "(SELECT DISTINCT i.id FROM MOVIES i " +
             "LEFT OUTER JOIN Running r ON i.id = r.movie.id AND r.startTime > :now) " +
-            "GROUP BY i.movie.id ORDER BY SUM(i.score) DESC")
+            "GROUP BY m.id ORDER BY AVG(r.score) DESC")
     List<Movie> findByRunningAndReleaseDate(LocalDateTime now);
+
+    // 영화 페이지, 전체 영화 리스트 출력, 기본 예매율 정렬
+    @Query("SELECT m.id, COUNT(m.id) from MOVIES m " +
+            "INNER JOIN Running r on m.id = r.movie.id " +
+            "AND r.startTime > :coming " +
+            "LEFT OUTER JOIN TICKETS t ON r.id = t.running.id " +
+            "GROUP BY m.id ORDER BY COUNT(m.id) DESC")
+    List<Object> findMoviesSortWithTickets(LocalDateTime coming);
 }
