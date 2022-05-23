4c95b1ad083947374f9b6eb990ab5e1b16e5a4c8
bodyMist <sky834459@gmail.com>
2022-05-23 20:19:28 +0900

    add movie repository test functions

--- src/test/java/choiscgvback/cgv/repository/MovieRepositoryTest.java
+++ src/test/java/choiscgvback/cgv/repository/MovieRepositoryTest.java
@@ -4,23 +4,39 @@ import choiscgvback.cgv.domain.Movie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 @SpringBootTest
 public class MovieRepositoryTest {
     @Autowired private MovieRepository movieRepository;
 
     @Test
     void 영화_상영_그래프_탐색(){
-        Optional<Movie> movie = movieRepository.findById(1L);
-        System.out.println(movie.get().getName());
+        List<Movie> movie = movieRepository.findAll();
+        movie.forEach(o -> System.out.println(o.getName() + " " + o.getRunningList()));
     }
     @Test
     void 메인페이지_영화출력_테스트(){
         List<Movie> movies = movieRepository.findByRunningAndReleaseDate(LocalDateTime.now());
         movies.stream().forEach(movie-> System.out.println(movie.getId()));
+    }
+
+    @Test
+    void 영화_리뷰_총점_테스트(){
+        Optional<Movie> movie = movieRepository.findById(1L);
+        System.out.println(movie.get().getScore());
+    }
+
+    @Test
+    void 영화_평점순_정렬(){
+        List<Movie> movies = movieRepository.findAll();
+        movies.stream().forEach(o -> {
+            System.out.println(o.getName() + " " + o.getScore());
+        });
     }
 }
