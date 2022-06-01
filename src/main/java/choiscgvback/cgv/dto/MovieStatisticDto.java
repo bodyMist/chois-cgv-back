456b2e034f0204e2c4b11d1ae61e025e3848955e
bodyMist <sky834459@gmail.com>
2022-06-01 15:37:14 +0900

    add the function of movie statistics

---
+++ src/main/java/choiscgvback/cgv/dto/MovieStatisticDto.java
@@ -0,0 +1,12 @@
+package choiscgvback.cgv.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MovieStatisticDto {
+    private boolean memberSex;
+    private int ageGroup;
+    private int count;
+}
--- src/main/java/choiscgvback/cgv/repository/TicketRepository.java
+++ src/main/java/choiscgvback/cgv/repository/TicketRepository.java
@@ -1,9 +1,21 @@
 package choiscgvback.cgv.repository;
 
 import choiscgvback.cgv.domain.Ticket;
+import choiscgvback.cgv.dto.MovieStatisticDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long>{
     // 예매를 위한 ticket 저장 & 예매취소
     Ticket saveAndFlush(Ticket newTicket);
+
+    // 영화 상세 페이지
+    // 특정 영화의 전체 예매 count, 예매에서 남여 각각에 대한 count, 나이대별 count
+    @Query("SELECT m.sex, m.age/10 as age_group, COUNT(t.id) FROM TICKETS t " +
+            "LEFT JOIN MEMBERS m on t.member.id = m.id " +
+            "INNER JOIN Running r on t.running.id = r.id AND r.movie.id = :movieId " +
+            "GROUP BY m.sex, (m.age/10)")
+    List<MovieStatisticDto> getStatistic(Long movieId);
 }
