b37b398576ea3a78e72c305025041db7f0a36bb4
bodyMist <sky834459@gmail.com>
2022-06-09 09:28:04 +0900

    add member page service

--- src/main/java/choiscgvback/cgv/service/MemberService.java
+++ src/main/java/choiscgvback/cgv/service/MemberService.java
@@ -1,19 +1,29 @@
 package choiscgvback.cgv.service;
 
 import choiscgvback.cgv.domain.Member;
+import choiscgvback.cgv.dto.MypageDto;
 import choiscgvback.cgv.repository.JpaRepository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements BaseService{
     private final MemberRepository memberRepository;
     public Member login(Member member){
         String account = member.getAccount();
         String password = member.getPassword();
         return memberRepository.findMemberByAccountEqualsAndPasswordEquals(account, password);
+    }
+
+    // 마이페이지
+    public MypageDto getMemberDetail(Long memberId){
+        Optional<Member> member = memberRepository.findById(memberId);
+        MypageDto mypage = new MypageDto(member.get());
+        return mypage;
     }
 }
